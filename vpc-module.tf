a2cae13458d3a96ae70bcbbbc63b9e0e30a45dba
Sankar491 <sivasankarrcks@gmail.com>
2024-09-30 15:02:58 +0530

    using github to call the module

--- vpc-module.tf
+++ vpc-module.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source      = "../Terraform-vpc-module"
+  # source      = "../Terraform-vpc-module"
+  source = "git::https://github.com/Sivasankar491/Terraform-vpc-module.git?ref=main"
   project     = var.project_name
   environment = var.environment_name
   common_tags = var.common_tags
